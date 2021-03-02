de69260acbb51328111b9414c983d8119b982296
oStatic440 <oreen.yousuf@gmail.com>
2021-03-02 20:26:12 +0100

    pushing new content from vlad repo to soon to be created repo

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="react-scripts" />
+
+declare module 'react-speech-kit';
+
+interface SDSContext {
+    recResult: string;
+    nluData: any;
+    ttsAgenda: string;
+    query: string;
+    snippet: string
+}
+
+type SDSEvent =
+    | { type: 'CLICK' }
+    | { type: 'RECOGNISED' }
+    | { type: 'ASRRESULT', value: string }
+    | { type: 'ENDSPEECH' }
+    | { type: 'LISTEN' }
+    | { type: 'SPEAK', value: string };
